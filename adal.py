4a72a328b481f2cab8a512a471d0999252a5b296
MattiMatt8 <momatthiasson@gmail.com>
2017-11-28 12:32:20 +0000

    Uppfærði aðal python skjalið

--- adal.py
+++ adal.py
@@ -16,10 +16,47 @@ for x in range(5):
     nagdyrin[x].prenta()
 
 print()
-nagdyrin[0].prenta()
-nagdyrin[0] = nagdyrin[0].nyStadsetning(100)
-nagdyrin[0].prenta()
+#nagdyrin[0].prenta()
+#nagdyrin[0] = nagdyrin[0].nyStadsetning(100)
+#nagdyrin[0].prenta()
 
+nagdyrin[3].prenta()
+nagdyrin[3] = nagdyrin[3].nyStadsetning(3)
+nagdyrin[3].prenta()
+mus = 0
+def rotturStad(kast):
+    haetta = False
+    tilBaka = False
+    tilBakaUm = 0
+    if tilBaka == False:
+        fra = nagdyrin[mus].stadsetning()
+        til = nagdyrin[mus].stadsetning()+kast+1
+        um = 1
+    else:
+        print("TIL BAKA")
+        fra = nagdyrin[mus].stadsetning()
+        til = nagdyrin[mus].stadsetning() - tilBakaUm + 1
+        um = -1
+    for x in range(fra,til,um):
+        print(x)
+        for y in range(2, 5):
+            rotta = nagdyrin[y].stadsetning()
+            if x == rotta:
+                print("Rotta", rotta, "er á reyti", x)
+                if nagdyrin[mus].afl() < nagdyrin[y].afl():
+                    haetta = True
+                    tilBaka = True
+                    tilBakaUm = nagdyrin[y].afl()
+                elif nagdyrin[mus].afl() == nagdyrin[y].afl():
+                    haetta = True
+        if haetta == True:
+            if tilBaka == True:
+                print()
+            break
 keyrsla = True
 while keyrsla != False:
+    kast = nagdyrin[mus].teningur()
+    nyrReytur = kast + nagdyrin[mus].stadsetning()
+    print("Hann kastaði",kast)
+    rotturStad(kast)
     break
