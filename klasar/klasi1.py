a1675f597feb24a16eca4107c68574b30127dc6d
odinnben <odinnben2000@gmail.com>
2017-11-28 12:32:59 +0000

    gerði teningin

--- klasar/klasi1.py
+++ klasar/klasi1.py
@@ -24,3 +24,4 @@ class nagdyr:
         return nagdyr(self.tegundin,ny,self.aflid)
 
 
+
--- klasar/klasi2.py
+++ klasar/klasi2.py
@@ -1,6 +1,43 @@
 # Óðinn og Matti
 # 21.11.2017
 from random import *
-class test2:
-    def __init__(self):
-        print("test2")
+class teningur:
+    def __init__(self,kast):
+        if kast == 1:
+            print("▉▉▉▉▉▉▉")
+            print("▉\t\t▉")
+            print("▉\t○\t▉")
+            print("▉\t\t▉")
+            print("▉▉▉▉▉▉▉")
+        if kast == 2:
+            print("▉▉▉▉▉▉▉")
+            print("▉○\t\t▉")
+            print("▉\t\t▉")
+            print("▉\t   ○▉")
+            print("▉▉▉▉▉▉▉")
+        if kast == 3:
+            print("▉▉▉▉▉▉▉")
+            print("▉○\t\t ▉")
+            print("▉   ○\t ▉")
+            print("▉   \t○▉")
+            print("▉▉▉▉▉▉▉")
+        if kast ==4:
+            print("▉▉▉▉▉▉▉")
+            print("▉○\t   ○▉")
+            print("▉\t\t ▉")
+            print("▉○\t   ○▉")
+            print("▉▉▉▉▉▉▉")
+        if kast ==5:
+            print("▉▉▉▉▉▉▉")
+            print("▉○\t   ○▉")
+            print("▉\t○\t▉")
+            print("▉○\t   ○▉")
+            print("▉▉▉▉▉▉▉")
+        if kast == 6:
+            print("▉▉▉▉▉▉▉")
+            print("▉○\t   ○▉")
+            print("▉○\t   ○▉")
+            print("▉○\t   ○▉")
+            print("▉▉▉▉▉▉▉")
+        print("teningur")
+
